70055be9c1e709bcafcbbf9ee389ac4bd949ca33
Pigf00d <thesolarpulse@gmail.com>
2025-05-29 15:58:56 -0400

    cleaning up:

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import LandingPage from "../views/LandingPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
