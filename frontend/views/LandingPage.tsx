36e02937a9190d17fd178e4348bcded6bf1da465
Pigf00d <thesolarpulse@gmail.com>
2025-05-30 21:01:04 -0400

    landing page complete

--- frontend/views/LandingPage.tsx
+++ frontend/views/LandingPage.tsx
@@ -1,12 +1,36 @@
 import "../styles/LandingPage.css";
+import { useState } from "react";
 
 export default function LandingPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="landing-page">
       <h1 className="WHOAMI-Header">WHOAMI</h1>
-      <input value="email" className="input"></input>
-      <input value="password" className="input"></input>
-      <button>LOGIN</button>
+      <form className="login-form">
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+          className="input"
+        ></input>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+          className="input"
+        ></input>
+        <button
+          onClick={() => {
+            setEmail("");
+            setPassword("");
+          }}
+        >
+          LOGIN
+        </button>
+      </form>
     </div>
   );
 }
